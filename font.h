366730f8874ee45f1fab4760f3dc4e7dd6d746ae
Devin Smith <devin@devinsmith.net>
2019-04-28 20:42:52 -0700

    Getting the default system font.

---
+++ font.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2000-2019 Devin Smith <devin@devinsmith.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef XPFONT_H
+#define XPFONT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct xpFont {
+  char faceName[64];
+  int height;
+};
+
+void xp_font_init(void);
+const struct xpFont *get_default_font(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XPFONT_H */
